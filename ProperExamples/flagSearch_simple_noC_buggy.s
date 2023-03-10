0dd34580fc60f955ddabd19ef6b074983efd7b21
Simón Gutiérrez Brida <sgutierrez@dc.exa.unrc.edu.ar>
2023-03-10 15:41:00 +0800

    Updated

---
+++ ProperExamples/flagSearch_simple_noC_buggy.s
@@ -0,0 +1,32 @@
+#flagSearch_simple_noC_buggy.s
+#
+#A simple program that given a value and some flags
+#will return as exit code which flags it found.
+#
+#Assemble as: gcc -c flagSearch_simple_noC_buggy.s
+#Link as: ld flagSearch_simple_noC_buggy.o -o flagSearch_simple_noC_buggy
+#Run as: ./flagSearch_simple_noC_buggy ; echo $?
+
+  .global _start
+  .type _start function
+  
+.text
+
+.equ FLAG1, 1       #00000001
+.equ FLAG2, 2       #00000010
+.equ FLAG4, 4       #00000100
+.equ FLAG8, 8       #00001000
+
+_start:
+  movzbq value2, %rdi  # Select value to use
+  movb $FLAG1, %sil   # All flags to search
+  or $FLAG8, %rsi     # you can add more using or
+  and %rdi, %rsi
+  # There is no need to again move the exit code to rdi
+  mov $60, %rax
+  syscall
+
+.data
+value1: .byte 7 #00000111
+value2: .byte 4 #00000100
+value3: .byte 43 #00101011
