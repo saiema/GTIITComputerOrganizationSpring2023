7c5c576a4217f83ab573b3a7426981da67525e28
Simón Gutiérrez Brida <sgutierrez@dc.exa.unrc.edu.ar>
2023-05-26 09:49:50 +0800

    Filtering books, third assignment for Computer Organization (TODO: move this to a proper folder)

--- ProperExamples/demosTutorial8/filterBooks.c
+++ ProperExamples/demosTutorial8/filterBooks.c
@@ -1,43 +1,104 @@
+/*
+Books filtering app.
+
+This program read books from a file and print out those that match a specific author.
+
+Arguments:
+
+The book's file: A file containing one line per book, ending with a new line character, and with the format: string,string,int,int,int
+                 Strings can be of up to 40 characters, and they don't require quotes (simple or double).
+
+The author to filter: The full name of an author, this name will be used as is and the filtering is case sensitive.
+*/
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
 
+#define STRING_MAX 40
+#define BOOKS_MAX 40
+
+/*
+Definition of Book, the isLended field is considered as a boolean value (0 for False, not 0 for True)
+*/
 typedef struct Book {
-    char * author;
-    char * tittle;
+    char title[STRING_MAX];
+    char author[STRING_MAX];
     int pages;
     int id;
     int isLended;
 } Book;
 
-extern Book * filterBooksByAuthor(Book books[], int size, char * author);
-extern unsigned int countFilteredBooksByAuthor(Book books[], int size, char * author);
-void printBook(Book book);
+/*
+Given an array of books, a size, and a string representing a particular author.
+This function will return an array of pointers to those books that belong to the requested author.
+The returned array must end with a 0 to denote it's ending
+*/
+extern Book ** filterBooksByAuthor(Book * books, int size, char * author);
 
-void main(void) {
-    Book b1 = {"Isaac Asimov", "The Caves of Steel", 224, 42, 0};
-    Book b2 = {"Isaac Asimov", "The Naked Sun", 187, 43, 0};
-    Book b3 = {"Isaac Asimov", "Mirror Image", 82, 101, 1};
-    Book b4 = {"Isaac Asimov", "The Robots of Dawn", 419, 44, 1};
-    Book b5 = {"Isaac Asimov", "Robots and Empire", 383, 45, 0};
-    Book b6 = {"J. R. R. Tolkien", "The Hobbit", 310, 62, 0};
-    Book b7 = {"J. R. R. Tolkien", "The Lord of the Rings", 9250, 63, 1};
-    Book b8 = {"J. R. R. Tolkien", "The Adventures of Tom Bombadil", 304, 64, 0};
-    Book b9 = {"J. R. R. Tolkien", "The Silmarillion", 365, 65, 0};
-    Book b10 = {"Douglas Adams", "The Hitchhiker's Guide to the Galaxy", 400, 72, 1};
-    Book myBooks[10] = {b1, b2, b3, b4, b5, b6, b7, b8, b9, b10};
-    unsigned int iasimovBooksCount = countFilteredBooksByAuthor(myBooks, 10, "Isaac Asimov");
-    Book * iasimovBooks = filterBooksByAuthor(myBooks, 10, "Isaac Asimov");
-    printf("There are %d books of Isaac Asimov\nBooks:\n", iasimovBooksCount);
-    for (int b = 0; b < iasimovBooksCount; b++) {
-        printBook(iasimovBooks[b]);
-        if (b + 1 < iasimovBooksCount) {
-            printf("\n");
-        }
+/*
+Prints a Book into standard output
+*/
+void printBook(const Book * book);
+
+/*
+Given a file name, and one already created array.
+This function will parse books from the file and stored them in the provided array.
+The result is how many books were loaded, or -1 if there was an error when opening the file.
+*/
+int parseBooksFromFile(char * file, Book books[BOOKS_MAX]);
+
+int main(int argc, char ** argv) {
+    if (argc != 3) {
+        printf("Wrong usage\nUsage: ./filterBooks <path to books file> <author to filter>\n");
+        return 1;
     }
+    char * booksFile = argv[1];
+    char * authorToFilter = argv[2];
+    Book myBooks[BOOKS_MAX];
+    int booksRead = parseBooksFromFile(booksFile, myBooks);
+    if (booksRead < 0) {
+        printf("An error occurred while trying to open file %s\n", booksFile);
+        return 2;
+    }
+    printf("Books read: %d\n", booksRead);
+    Book ** authorBooks = filterBooksByAuthor(myBooks, booksRead, authorToFilter);
+    printf("Filtered books for author %s:\n", authorToFilter);
+    int current = 0;
+    while (authorBooks[current]) {
+        printBook(authorBooks[current]);
+        current++;
+        if (authorBooks[current]) printf("\n");
+    }
+    printf("Amount of filtered books for author %s: %d\n", authorToFilter, current);
+    return 0;
 }
 
-void printBook(Book book) {
+void printBook(const Book * book) {
     printf("Author: %s\nTitle: %s\nPages: %d\nID: %d\nIs Lended: %s\n",
-        book.author, book.tittle, book.pages, book.id, book.isLended?"TRUE":"FALSE"
+        book->author, book->title, book->pages, book->id, book->isLended?"TRUE":"FALSE"
     );
+}
+
+int parseBooksFromFile(char * file, Book books[BOOKS_MAX]) {
+    FILE* filepointer = fopen(file, "r");
+    if (!filepointer) {
+        return -1;
+    }
+    int ret = 0;
+    int i = 0;
+    while (ret != EOF && i < BOOKS_MAX) {
+        ret = fscanf(
+            filepointer,
+            "%40[^,],%40[^,],%d,%d,%d\n",
+            books[i].author,
+            books[i].title,
+            &books[i].pages,
+            &books[i].id,
+            &books[i].isLended
+        );
+        if (ret != EOF) {
+            i++;
+        }
+    }
+    return i;
 }
--- ProperExamples/demosTutorial8/filterBooks_functions.s
+++ ProperExamples/demosTutorial8/filterBooks_functions.s
@@ -1,47 +1,20 @@
 .global filterBooksByAuthor
-.global countFilteredBooksByAuthor
 .type filterBooksByAuthor function
-.type countFilteredBooksByAuthor function
 
 .text
 
-
-countFilteredBooksByAuthor:
-    push %rbp
-    mov %rsp, %rbp
-    mov %rsi, %rax
-    leave
-    ret
-
-filterBooksByAuthor:
-    push %rbp
-    mov %rsp, %rbp
-    mov %rdi, %rax
-    leave
-    ret
-
-    /*push %rdi           # the array of books    (8)
-    push %rsi           # the size of the array (16)
-    push %rdx           # the author to filter  (24)
-    push $0             # index                 (32)
-    push $0             # count                 (40)
-    jmp countFilteredBooksByAuthor_loop_test
-
-countFilteredBooksByAuthor_loop_body:
-    mov -8(%rbp), %r8
-    mov -32(%rbp), %r9
-    
-
-countFilteredBooksByAuthor_loop_test:
-    mov -40(%rbp), %r8
-    mov -16(%rbp), %r9
-    cmp %r8, %r9
-    jg countFilteredBooksByAuthor_loop_body
-
-countFilteredBooksByAuthor_done:
-    mov -40(%rbp), %rax
-    add $40, %rsp
-    leave
-    ret
+/*
+ .EQU BOOK_SIZE, ???
+ .EQU AUTHOR_OFFSET, ???
 
 */
+/*
+Given an array of books, a size, and a string representing a particular author.
+This function will return an array of pointers to those books that belong to the requested author.
+The returned array must end with a 0 to denote it's ending
+
+The returned array must be allocated into the heap, and it will have one more element than the amount of
+books returned to allocate the 0 at the end.
+*/
+filterBooksByAuthor:
+    /*TODO: implement*/
