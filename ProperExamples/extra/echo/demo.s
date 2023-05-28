615d6423e249690ca9b89181dd21f21173ff18aa
Simón Gutiérrez Brida <sgutierrez@dc.exa.unrc.edu.ar>
2023-05-28 17:58:31 +0800

    Added echo no-PIE implementation, to use as integrating example in Digital Systems

---
+++ ProperExamples/extra/echo/demo.s
@@ -0,0 +1,96 @@
+/*
+A simple echo program.
+This program will print the amount of arguments in argv and print them (including the executable path).
+To compile this program execute:
+1. Generate the static library astd (this only need to be generated once unless a change is made)
+    gcc [-ggdb] -c -z noexecstack string.s -o string.o
+    gcc [-ggdb] -c -z noexecstack stdutils.s -o stdutils.o
+    ar rcs libastd.a stdutils.o string.o
+2. Compile the executable
+    gcc [-ggdb] -z noexecstack demo.s -L "$PWD" -l astd -o demo
+
+To run the program you need to execute:
+    ./demo (string)*
+
+note 1: [-ggdb] stands for: "optionally you can write -ggdb"
+note 2: (string)* stands for: "zero or more strings"
+
+example:
+    ./demo "This" "are" "the" "arguments"
+    
+    will print
+    
+    Number of arguments: 5
+    Argument[0]: ./demo
+    Argument[1]: This
+    Argument[2]: are
+    Argument[3]: the
+    Argument[4]: arguments
+*/
+
+
+.global main
+.type main function
+.extern print_integer
+.extern print_string
+.type print_integer function
+.type print_string function
+.extern _exit
+.type _exit function
+
+.text
+
+main:
+    push %rbp
+    mov %rsp, %rbp
+    push %rdi # argc (%rbp - 8)
+    push %rsi # argv (%rbp - 16)
+    push $0   # index (i) (%rbp - 24)
+    mov $nargsmsg, %rdi
+    call print_string
+    mov -8(%rbp), %rdi
+    mov $1, %rsi            /*  <--------
+                            The second argument of print_integer
+                            will define wether or not to print a
+                            new-line character after the number
+                            */
+    call print_integer
+    mov $newline, %rdi
+    call print_string
+    jmp printArguments_test
+
+/*
+This subroutine will print "Argument[i]: argv[i]"
+*/
+printArguments:
+    mov $argmsg_init, %rdi
+    call print_string
+    mov -24(%rbp), %rdi
+    mov $0, %rsi
+    call print_integer
+    mov $argmsg_end, %rdi
+    call print_string
+    mov -16(%rbp), %r8      # argv
+    mov -24(%rbp), %r9      # i
+    mov %r9, %rdi
+    mov (%r8, %r9, 8), %rdi # argv[i] (this address is calculated by doing argv + (i * 8))
+    call print_string
+    mov $newline, %rdi
+    call print_string
+    incq -24(%rbp)          # i++
+
+
+printArguments_test:
+    mov -8(%rbp), %r8
+    cmp %r8, -24(%rbp)
+    jnz printArguments
+
+done:
+    mov $0, %rdi
+    call _exit
+
+.data
+nargsmsg: .asciz "Number of arguments: "
+newline: .asciz "\n"
+argmsg_init: .asciz "Argument["
+argmsg_end: .asciz "]: "
